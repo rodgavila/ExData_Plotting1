35d11805f9b4704c853070d3eb461807b2a38c00
Rodrigo Gomez Avila <rodgavila@hotmail.com>
2016-01-07 01:38:26 -0800

    Directly use hpc.data$DateTime instead of generating custom axis

--- plot2.R
+++ plot2.R
@@ -3,7 +3,6 @@ source("plotcomm.R")
 plot2 <- function() {
   hpc.data <- load.hpc.data() # See plotcomm.R
   png(filename="plot2.png")
-  plot(hpc.data$Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xaxt="n", xlab="")
-  axis(1, at=c(1, 1440, 2880), labels=c("Thu", "Fri", "Sat"))
+  plot(hpc.data$DateTime, hpc.data$Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xlab="")
   dev.off()
 }
